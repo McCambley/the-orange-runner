bfdeaa8a1c61273bda3337c9d0d773265fccf4b0
McCambley <jakemccamb16@gmail.com>
2022-01-17 10:43:29 -0500

    Dev: impliment shimmer on story component

--- components/Story.js
+++ components/Story.js
@@ -1,17 +1,22 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { BLOCKS } from "@contentful/rich-text-types";
+import { generateShimmer } from "../lib/shimmer";
 import Image from "next/image";
 
 export default function Story({ comic }) {
   const renderOption = {
     renderNode: {
       [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+        const { url, details } = node.data.target.fields.file;
+        const { height, width } = details.image;
         return (
           <Image
-            src={`https:${node.data.target.fields.file.url}`}
-            height={node.data.target.fields.file.details.image.height}
-            width={node.data.target.fields.file.details.image.width}
+            src={`https:${url}`}
+            height={height}
+            width={width}
             alt="Illustration"
+            placeholder="blur"
+            blurDataURL={generateShimmer(width, height)}
           />
         );
       },
