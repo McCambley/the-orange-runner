b07083621d2ea8abb9ebe1d0d809fbde1aa891de
McCambley <jakemccamb16@gmail.com>
2022-01-17 19:55:05 -0500

    Dev: format font styles

--- components/Layout.js
+++ components/Layout.js
@@ -18,6 +18,12 @@ export default function Layout({ children, home }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Serif:ital@0;1&family=Source+Sans+Pro:wght@700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Header>
         {home ? (
--- styles/Global.js
+++ styles/Global.js
@@ -5,70 +5,78 @@ export const GlobalStyle = createGlobalStyle`
 
 ${normalize};
 
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap');
-/* font-family: 'Source Sans Pro', sans-serif; */
-@import url('https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-/* font-family: 'PT Serif', serif; */
-
 
 * {
-box-sizing: border-box;
+    box-sizing: border-box;
+    font-family: 'PT Serif', serif;
+    font-weight: 900;
+
 }
+
 body {
-/* background: rgba(255,153,0,1); */
-
-/* temporary */
-display: flex;
-justify-content: center;
-align-items: center;
-min-height: 100vh;
-padding: 16px;
-/* default Next font taken from tutorial */
-
-
-h1 {
-font-weight: 700;
-line-height: 1.4em;
-margin: 0;
-font-weight: 900;
-
-
+    /* background: rgba(255,153,0,1); */
+    
+    /* temporary */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    padding: 16px;
+    /* default Next font taken from tutorial */
+    
+    
+    h1 {
+        font-weight: 700;
+        line-height: 1.4em;
+        margin: 0;
+        font-weight: 700;
+        font-family: 'Source Sans Pro', sans-serif; 
+        font-weight: 900;
 }
+
 h2 {
     margin: 0;
-    font-weight: 900;
+    font-family: 'Source Sans Pro', sans-serif; 
+        font-weight: 900;
 }
 h3 {
     margin: 0;
     opacity: .5;
+    font-family: 'Source Sans Pro', sans-serif; 
+        font-weight: 900;
+}
+h4 {
+    margin: 0;
+    font-family: 'Source Sans Pro', sans-serif; 
+        font-weight: 900;
 }
 h5 {
     margin: 0;
+    font-family: 'Source Sans Pro', sans-serif; 
+        font-weight: 900;
 }
 h6 {
     margin: 0;
+    font-family: 'Source Sans Pro', sans-serif; 
+        font-weight: 900;
 }
 p {
     margin: 0;
     font-weight: 400;
+    font-family: 'PT Serif', serif;
  
 }
 a {
     text-decoration: none;
-    color: inherit;
+    color: #000;
     cursor: pointer;
-    opacity: 1;
     transition: opacity .3s ease;
-
+    font-family: 'PT Serif', serif;
+    font-weight: 400;
+    transition: color .15s ease;
+    
     &:hover {
-        opacity: .7;
+        color: #FF9900;
     }
-}
-
-h1, h2, h3, h4, h5, h6, p {
-/* temp */
-text-align: center;
-width: 100%;
-margin-bottom: 12px;
 }
 `;
