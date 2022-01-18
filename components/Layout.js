35b5741e20bc9c4d0fc654199a120e58228a743b
McCambley <jakemccamb16@gmail.com>
2022-01-18 16:46:39 -0500

    Dev: import bold pt serif

--- components/Layout.js
+++ components/Layout.js
@@ -18,7 +18,7 @@ export default function Layout({ children, home }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=PT+Serif:ital@0;1&family=Source+Sans+Pro:wght@400;700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Source+Sans+Pro:wght@400;700;900&display=swap"
           rel="stylesheet"
         />
       </Head>
--- styles/styledAbout.js
+++ styles/styledAbout.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.section`
   a {
     color: #ff9900;
+    /* font-weight: 700; */
   }
 
   @media (max-width: 768px) {
