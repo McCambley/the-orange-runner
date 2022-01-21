9cd276e9b61acadf9d2bb60eee5e6b062a89285f
McCambley <jakemccamb16@gmail.com>
2022-01-21 08:59:44 -0500

    Fix: remove priority from logos to allow for lazy loading

--- components/Header.js
+++ components/Header.js
@@ -15,18 +15,18 @@ export default function Header() {
       <Wrapper $isOpen={isOpen}>
         <Link href="/" passHref>
           <LargeLogo>
-            <Image priority src="/images/logo-header.jpeg" height={1536} width={1536} alt="The Orange runner" />
+            <Image src="/images/logo-header.jpeg" height={1536} width={1536} alt="The Orange runner" />
           </LargeLogo>
         </Link>
         <StickyHeader>
           <Link href="/" passHref>
             <SmallLogo>
-              <Image priority src="/images/logo.png" height={512} width={512} alt="The Orange runner" />
+              <Image src="/images/logo.png" height={512} width={512} alt="The Orange runner" />
             </SmallLogo>
           </Link>
           <Link href="/" passHref>
             <a>
-              <Image priority src="/images/the-orange-runner.png" height={67} width={478} alt="The Orange runner" />
+              <Image src="/images/the-orange-runner.png" height={67} width={478} alt="The Orange runner" />
             </a>
           </Link>
           <Hamburger type="button" onClick={toggleOpen} $isOpen={isOpen} />
