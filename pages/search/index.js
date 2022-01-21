4451f152115eaa7c109dcfcd4bfefc4f79f98349
McCambley <jakemccamb16@gmail.com>
2022-01-21 18:24:01 -0500

    Dev: import createClient to search page

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,6 +2,12 @@ import { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import { useRouter } from "next/router";
 import Loading from "../../components/Loading";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -13,12 +19,21 @@ export default function Search() {
 
   useEffect(() => {
     setLoading(true);
-    fetch("api/profile-data")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
+
+    // client
+    //   .getEntries({
+    //     content_type: "comic",
+    //   })
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((err) => console.error(err));
+    // fetch("api/profile-data")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setData(data);
+    //     setLoading(false);
+    //   });
   }, []);
 
   return <Layout>{true && <Loading />}</Layout>;
