1bde5dc153b66e5e5fc8745a79f6b5ca23b56843
McCambley <jakemccamb16@gmail.com>
2022-01-23 23:17:29 -0500

    Ref: wrap 404 in layout

--- pages/404.js
+++ pages/404.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { Wrapper, Title, Message, Safety } from "../styles/styledNotFound";
+import Layout from "../components/Layout";
 
 export default function NotFound() {
   const router = useRouter();
@@ -14,12 +15,14 @@ export default function NotFound() {
   }, []);
 
   return (
-    <Wrapper>
-      <Title>404</Title>
-      <Message>{"Oops! We're lost."}</Message>
-      <Link href="/" passHref>
-        <Safety>&larr; Head back home </Safety>
-      </Link>
-    </Wrapper>
+    <Layout>
+      <Wrapper>
+        <Title>404</Title>
+        <Message>{"Oops! We're lost."}</Message>
+        <Link href="/" passHref>
+          <Safety>&larr; Head back home </Safety>
+        </Link>
+      </Wrapper>
+    </Layout>
   );
 }
