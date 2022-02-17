5ad98577e2edc002f6442b6a01e9d6f06a5478cf
McCambley <jakemccamb16@gmail.com>
2022-02-17 00:16:39 -0500

    Dev: create basic hamburger animation

--- components/Hamburger.js
+++ components/Hamburger.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { Button } from "../styles/styledHamburger";
+import { Button, Top, Middle, Bottom } from "../styles/styledHamburger";
 
 export default function Hamburger({ handleClick, isOpen }) {
   return (
     <Button type="button" onClick={handleClick} $isOpen={isOpen}>
-      cool
+      <Top $isOpen={isOpen} />
+      <Middle $isOpen={isOpen} />
+      <Bottom $isOpen={isOpen} />
     </Button>
   );
 }
--- styles/styledHamburger.js
+++ styles/styledHamburger.js
@@ -7,15 +7,43 @@ export const Button = styled.button`
     display: block;
     background-color: transparent;
     border: none;
-    background-image: url(${(props) => (props.$isOpen ? "/images/close.svg" : "/images/open.svg")});
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center;
-    /* transition: background-image 0.15s ease; */
+    position: relative;
+    /* background-image: url(${(props) => (props.$isOpen ? "/images/close.svg" : "/images/open.svg")}); */
+    /* background-size: cover; */
+    /* background-repeat: no-repeat; */
+    /* background-position: center; */
+    transition: background-image 0.15s ease;
     cursor: pointer;
     height: 0;
     width: 100%;
     padding-top: 100%;
     user-select: none;
+    outline: 1px solid red;
   }
+`;
+
+const Span = styled.span`
+  position: absolute;
+  width: 75%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 0;
+  border: 4px solid #000;
+  border-radius: 20px;
+  transition: transform 0.3s ease;
+`;
+
+export const Top = styled(Span)`
+  transform: ${({ $isOpen }) => ($isOpen ? "translate(-50%, -50%)" : "translate(-50%, -300%)")};
+  /* transform: translate(-50%, -300%); */
+`;
+
+export const Middle = styled(Span)`
+  transform: translate(-50%, -50%);
+`;
+
+export const Bottom = styled(Span)`
+  transform: ${({ $isOpen }) => ($isOpen ? "translate(-50%, -50%)" : "translate(-50%, 200%)")};
+  /* transform: translate(-50%, 200%); */
 `;
