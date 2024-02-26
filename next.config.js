331180f8d6d0d0807b4d69e4fcca369c5cb62126
McCambley <jakemccamb16@gmail.com>
2024-02-26 08:59:33 -0500

    Add unoptimized image support and update dependencies

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ["images.ctfassets.net"],
+    // Temporarily unoptimizing to minimize costs
+    unoptimized: true,
   },
   i18n: {
     locales: ["en"],
